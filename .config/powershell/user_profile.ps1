fd5bbbac9945c45df2b83df3fd49da22235e6f7f
Sutsikii <nicolas-dutertre@bbox.fr>
2023-01-07 12:41:47 +0100

    MajUserProfil

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,4 +1,6 @@
 # Alias 
-Set-Alias vim nvim
+Set-Alias vim nvim 
 Set-Alias ll ls
 Set-Alias g git
+Set-Alias push 'C:\Users\nicol\Documents\Script\Git\push.bat'
+Set-Alias commit 'C:\Users\nicol\Documents\Script\Git\commit-all.bat'
